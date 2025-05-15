76f50bc98ffd20278475ffc851c33e0c8b21165d
Gamerking177 <rajaksh03@gmail.com>
2025-05-15 18:47:15 +0530

    error fixed but in locally

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-// ✅ Import your JS files here (as side-effect imports)
-import "./js/gsap.min.js";
-import "./js/imagesloaded.pkgd.min.js";
-import "./js/smoothscroll.js";
-import "./js/utils.js";
+
 import './js/index.js'
 import Gallery from './components/Gallery.jsx'
 
